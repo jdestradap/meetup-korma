e6deef209597de9b2c29d3aca0577be12c0cab0a
jdestradap <jdestradap@gmail.com>
2016-04-14 13:00:57 -0500

    configuracion migratus.

---
+++ profiles.clj
@@ -0,0 +1,1 @@
+{:profiles/dev  {:env {:database-url "jdbc:postgresql://localhost/korma?user=korma&password=kormapass"}}}
--- project.clj
+++ project.clj
@@ -6,7 +6,21 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [korma "0.4.2"]
                  [org.postgresql/postgresql "9.4-1206-jdbc4"]
-                 [migratus "0.8.13"]]
+                 [migratus "0.8.13"]
+                 [org.slf4j/slf4j-log4j12 "1.7.9"]
+                 [environ "1.0.2"]]
+
   :main ^:skip-aot meetup-clojure.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :migratus {:store :database}
+  :plugins [[migratus-lein "0.2.1"]
+            [lein-environ "1.0.2"]]
+  :profiles 
+  {:uberjar {:aot :all}
+   :dev [:project/dev :profiles/dev]
+   :project/dev {:repl-options {:init-ns user}
+                 :env {:dev  true
+                       :port 3000
+                       :nrepl-port 7000}}
+   :profiles/dev {}
+   })
