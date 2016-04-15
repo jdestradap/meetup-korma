71d3ea04c0bab5005995c8679bfffae3448f67a8
jdestradap <jdestradap@gmail.com>
2016-04-14 23:48:44 -0500

    select Cheese user

--- src/meetup_clojure/db/core.clj
+++ src/meetup_clojure/db/core.clj
@@ -1,10 +1,15 @@
 (ns meetup-clojure.db.core
   (:require
    [korma.db :refer :all]
+   [korma.core :refer :all]
    [clojure.string :as str])
   )
 
-(defdb korma-db (postgres {:db "korma"
+(defdb korma-db (postgres {:db "korma-db"
                            :user "korma"
                            :password "kormapass"
                            }))
+(println "sss")
+
+(defentity users)
+(select users)
